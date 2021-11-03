a7887d66d65542be52f09ec8482c00ec6f120844
takuto <takutoitoi914@ruri.waseda.jp>
2021-11-03 18:22:45 +0900

    changed author name to me

--- webpack.inline.js
+++ webpack.inline.js
@@ -1,24 +1,25 @@
-const HtmlInlineScriptPlugin = require('html-inline-script-webpack-plugin');
+const HtmlInlineScriptPlugin = require("html-inline-script-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
 
 module.exports = merge(common, {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'ACG Homework',
-            meta: [
-                {
-                    viewport: 'width=device-width, initial-scale=1',
-                    name: 'author',
-                    content: 'NAME (STUDENT ID)'
-                }
-            ],
-            inlineSource: '.(glsl|ts|js|css)$',
-            minify: false,
-        }),
-        new HtmlInlineScriptPlugin()
-    ]
-})
+  mode: "development",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ACG Homework",
+      meta: [
+        {
+          viewport: "width=device-width, initial-scale=1",
+          name: "author",
+          content: "Takuto Itoi (1W183011-7)",
+        },
+      ],
+      inlineSource: ".(glsl|ts|js|css)$",
+      minify: false,
+    }),
+    new HtmlInlineScriptPlugin(),
+  ],
+});
+
